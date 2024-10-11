4cd0c51359a43c9faf97819a5762c2787530da1c
nascimento-le <lele.nascimento1117@gmail.com>
2024-10-10 22:34:16 -0300

    Inserindo X e O como opções

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -3,38 +3,45 @@
 import pygame as pg
 
 # pygame setup
-pg.init()
-screen = pg.display.set_mode((500, 500))
-pg.display.set_caption("Jogo da velha")
-clock = pg.time.Clock()
-running = True
-cor_fundo = 1 #azul
-#cor_fundo = 2 #vermelho
+pg.init() #iniciando o projeto
+pg.font.init() #Trazendo a inicialização da fonte utilizada no game
+screen = pg.display.set_mode((500, 500)) # Configura o tamanho da tela
+pg.display.set_caption("Jogo da velha") #Configura o nome da janela
+clock = pg.time.Clock() #biblioteca do tempo
+fonte_quadros = pg.font.SysFont("Comic Sans Ms", 30) #seleciono o tipo de fonte e tamanho para o meu parametro
+
+running = True 
+cor_fundo = 1 #parametro para cor inicial
+
+jogador1 = fonte_quadros.render("X", True, "black")
+jogador2 = fonte_quadros.render("O", True, "black")
+
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # controle de eventos do jogo
+    # pygame.QUIT evento que ao clicar no X encerra a janela
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-        if event.type == pg.MOUSEBUTTONDOWN:
+        if event.type == pg.MOUSEBUTTONDOWN: #evento que indica que o botão foi clicado
             print("Clicou")
             cor_fundo += 1
-            if(cor_fundo > 3):
+            if(cor_fundo > 3): #se clicarmos mais que 3x aparecerá a primeira opção de cor, como um looping 
                 cor_fundo = 1
 
-
     if cor_fundo == 1:
-        screen.fill("blue")
+        #screen.fill("blue") #primeiro clique aparece esta primeira cor
+        screen.blit(jogador1,(230, 250)) #quando eu selecionar, aparecerá o ícone do jogador 1
     elif cor_fundo == 2:
-        screen.fill("orange")
+        #screen.fill("orange") #segundo clique aparece esta segunda cor
+        screen.blit(jogador2,(290,250)) #quando eu selecionar, aparecerá o ícone do jogador 2
     else:
-        screen.fill("purple")
+        screen.fill("purple") #terceiro clique aparece esta terceira cor
 
     # flip() the display to put your work on screen
     pg.display.flip()
     
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60)  # limita FPS para 60
 
 pg.quit()
